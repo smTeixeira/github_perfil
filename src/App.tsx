a7cccca858c664578bf44217c34d1ac50d899f17
samuel de Matos Teixeira <samuelteixeira00@hotmail.com>
2023-11-04 14:58:28 -0300

    corrigindo erro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from "react"
 
 import Perfil from "./components/Perfil"
-import Formulario from "./components/Formulario"
 import ReposList from "./components/ReposList";
 
 function App() {
-  const [fomularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
   const [nomeUsuario, setNomeUsuario] = useState('')
 
 
